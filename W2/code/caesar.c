31632639176ddf350f51ff6a420d6eae6b9cb06a
Maksym Dmukhovskyy <dm.maksym@gmail.com>
2020-10-25 13:07:08 +0100

    caesar solution

--- W2/code/caesar.c
+++ W2/code/caesar.c
@@ -2,10 +2,19 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 
- int main(int argc, char* argv[])
- {
+int main(int argc, char *argv[])
+{
+
+    // Handle if there is no arguments or there are too many
+    if (!argv[1] || argv[2])
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+
     // Check if the provided argument is all integers
     string argument = argv[1];
     int argLength = strlen(argument);
@@ -14,7 +23,7 @@
     {
         bool isArgCharDigit = isdigit(argument[i]);
 
-        if(isArgCharDigit == false)
+        if (isArgCharDigit == false)
         {
             printf("Usage: ./caesar key\n");
             return 1;
@@ -22,7 +31,33 @@
     }
 
     // If the provided key is a number ask for the string to encrypt
-    string plainText = get_string("plaintext:");
+    string plainText = get_string("plaintext: ");
 
-    printf("%s\n", plainText);
- }
+    // Print the text block "ciphertext"
+    printf("ciphertext: ");
+
+
+    // Loop trough all letters in the provided text
+    int key = atoi(argv[1]);
+    int plainTextLength = strlen(plainText);
+    for (int i = 0; i < plainTextLength; i++)
+    {
+        // Check if it is a lowercase char between a and z
+        if (plainText[i] >= 'a' && plainText[i] <= 'z')
+        {
+            char encryptedChar = (((plainText[i] - 'a') + key) % 26) + 'a';
+            printf("%c", encryptedChar);
+        }
+        else if (plainText[i] >= 'A' && plainText[i] <= 'Z') // if it is a uppercase char between A and Z
+        {
+            char encryptedChar = (((plainText[i] - 'A') + key) % 26) + 'A';
+            printf("%c", encryptedChar);
+        }
+        else // Print other characters without encrypting
+        {
+            printf("%c", plainText[i]);
+        }
+    }
+    printf("\n");
+    return 0;
+}
